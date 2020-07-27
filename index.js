daec48814494dba2f97905dd656566317a129dad
shenxianpeng <xianpeng.shen@gmail.com>
2020-07-27 09:39:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const readme = `\
 
 ⏰ Updated on ${new Date().toUTCString()}
 
-![Progress Bar CI](https://github.com/shenxianpeng/shenxianpeng/workflows/Progress%20Bar%20CI/badge.svg)\
+![build](https://github.com/shenxianpeng/shenxianpeng/workflows/build/badge.svg)\
 `
 
 console.log(readme)
